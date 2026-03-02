a389d715f4b20b6ad1573fb40e183f468e215482
soulx <170918810@qq.com>
2026-03-02 21:54:07 +0800

    fix

--- src/main/java/jp/kitabatakep/intellij/plugins/codereadingnote/AppConstants.java
+++ src/main/java/jp/kitabatakep/intellij/plugins/codereadingnote/AppConstants.java
@@ -2,6 +2,6 @@ package jp.kitabatakep.intellij.plugins.codereadingnote;
 
 public class AppConstants
 {
-    public static final String appName = "CodeReadingNoteMarkPro";
+    public static final String appName = "CodeReadingNote";
     public static final String TOOL_WINDOW_ID = "Code Reading Mark Note Pro";
 }
